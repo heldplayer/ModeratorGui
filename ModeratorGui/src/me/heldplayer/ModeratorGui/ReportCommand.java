7512f3c3f81ed12c9ba714161661f8dba72488f7
heldplayer <mitchel.pyl@gmail.com>
2012-05-06 00:07:36 +0200

    Added player name check

--- ModeratorGui/src/me/heldplayer/ModeratorGui/ReportCommand.java
+++ ModeratorGui/src/me/heldplayer/ModeratorGui/ReportCommand.java
@@ -1,9 +1,14 @@
 package me.heldplayer.ModeratorGui;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import me.heldplayer.ModeratorGui.tables.Bans;
 import me.heldplayer.ModeratorGui.tables.Issues;
 import me.heldplayer.ModeratorGui.tables.Unbans;
 
+import org.bukkit.ChatColor;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -18,7 +23,31 @@ public class ReportCommand implements CommandExecutor {
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String alias, String[] args) {
-		if (args[0].equalsIgnoreCase("issue") && args.length > 2) {
+		if (args[0].equalsIgnoreCase("issue") && args.length > 2 && sender.hasPermission("moderatorgui.issue")) {
+			List<String> matchedNames = getPlayerMatches(args[1]);
+
+			String name = "";
+
+			if (matchedNames.size() == 1) {
+				name = matchedNames.get(0);
+			} else if (matchedNames.size() == 0) {
+				sender.sendMessage(ChatColor.RED + "No match found for '" + args[1] + "'");
+
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Multiple matches found: ");
+
+				String matches = "";
+
+				for (String matched : matchedNames) {
+					matches = ", " + matched;
+				}
+
+				matches = matches.replaceFirst(", ", "").trim();
+
+				return true;
+			}
+
 			String issue = "";
 
 			for (int i = 2; i < args.length; i++) {
@@ -29,16 +58,39 @@ public class ReportCommand implements CommandExecutor {
 
 			Issues issueRow = new Issues();
 			issueRow.setIssue(issue);
-			issueRow.setReported(args[1]);
+			issueRow.setReported(name);
 			issueRow.setReporter(sender.getName());
 			issueRow.setTimestamp(System.currentTimeMillis());
-			
+
 			main.getDatabase().save(issueRow);
-			
+
 			return true;
 		}
-		
-		if (args[0].equalsIgnoreCase("ban") && args.length > 2) {
+
+		if (args[0].equalsIgnoreCase("ban") && args.length > 2 && sender.hasPermission("moderatorgui.ban")) {
+			List<String> matchedNames = getPlayerMatches(args[1]);
+
+			String name = "";
+
+			if (matchedNames.size() == 1) {
+				name = matchedNames.get(0);
+			} else if (matchedNames.size() == 0) {
+				sender.sendMessage(ChatColor.RED + "No match found for '" + args[1] + "'");
+
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Multiple matches found: ");
+
+				String matches = "";
+
+				for (String matched : matchedNames) {
+					matches = ", " + matched;
+				}
+
+				matches = matches.replaceFirst(", ", "").trim();
+
+				return true;
+			}
 			String reason = "";
 
 			for (int i = 2; i < args.length; i++) {
@@ -49,16 +101,40 @@ public class ReportCommand implements CommandExecutor {
 
 			Bans banRow = new Bans();
 			banRow.setReason(reason);
-			banRow.setBanned(args[1]);
+			banRow.setBanned(name);
 			banRow.setBanner(sender.getName());
 			banRow.setTimestamp(System.currentTimeMillis());
-			
+
 			main.getDatabase().save(banRow);
-			
+
 			return true;
 		}
-		
-		if (args[0].equalsIgnoreCase("unban") && args.length > 2) {
+
+		if (args[0].equalsIgnoreCase("unban") && args.length > 2 && sender.hasPermission("moderatorgui.unban")) {
+			List<String> matchedNames = getPlayerMatches(args[1]);
+
+			String name = "";
+
+			if (matchedNames.size() == 1) {
+				name = matchedNames.get(0);
+			} else if (matchedNames.size() == 0) {
+				sender.sendMessage(ChatColor.RED + "No match found for '" + args[1] + "'");
+
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Multiple matches found: ");
+
+				String matches = "";
+
+				for (String matched : matchedNames) {
+					matches = ", " + matched;
+				}
+
+				matches = matches.replaceFirst(", ", "").trim();
+
+				return true;
+			}
+
 			String reason = "";
 
 			for (int i = 2; i < args.length; i++) {
@@ -69,16 +145,29 @@ public class ReportCommand implements CommandExecutor {
 
 			Unbans unbanRow = new Unbans();
 			unbanRow.setReason(reason);
-			unbanRow.setUnbanned(args[1]);
+			unbanRow.setUnbanned(name);
 			unbanRow.setUnbanner(sender.getName());
 			unbanRow.setTimestamp(System.currentTimeMillis());
-			
+
 			main.getDatabase().save(unbanRow);
-			
+
 			return true;
 		}
 
 		return false;
 	}
 
+	private List<String> getPlayerMatches(String name) {
+		OfflinePlayer[] players = main.getServer().getOfflinePlayers();
+
+		List<String> matched = new ArrayList<String>();
+
+		for (OfflinePlayer player : players) {
+			if (player.getName().substring(0, name.length()).equalsIgnoreCase(name)) {
+				matched.add(player.getName());
+			}
+		}
+
+		return matched;
+	}
 }
