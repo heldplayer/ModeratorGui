d2f24bb3d6276d61c2ae0a2e7076f15179feafd7
heldplayer <mitchel.pyl@gmail.com>
2012-05-06 18:48:02 +0200

    Colour coding

--- ModeratorGui/src/me/heldplayer/ModeratorGui/ReviewCommand.java
+++ ModeratorGui/src/me/heldplayer/ModeratorGui/ReviewCommand.java
@@ -27,7 +27,7 @@ public class ReviewCommand implements CommandExecutor {
 
 			String[] results = new String[13];
 
-			results[0] = ChatColor.GRAY + "Types: " + ChatColor.YELLOW + "Issue " + ChatColor.DARK_RED + "Ban " + ChatColor.DARK_GREEN + "Unban " + ChatColor.RED + "Promote " + ChatColor.GREEN + "Demote";
+			results[0] = ChatColor.GRAY + "Types: " + ChatColor.YELLOW + "Issue " + ChatColor.DARK_RED + "Ban " + ChatColor.DARK_GREEN + "Unban " + ChatColor.GREEN + "Promote " + ChatColor.RED + "Demote";
 			results[1] = ChatColor.GRAY + "Current time: " + dateFormat.format(Long.valueOf(System.currentTimeMillis()));
 			results[2] = ChatColor.GRAY + "" + ChatColor.ITALIC + "All times are MM-dd-yyyy HH:mm:ss";
 
@@ -46,27 +46,27 @@ public class ReviewCommand implements CommandExecutor {
 				case ISSUE:
 					Issues issue = main.getDatabase().find(Issues.class).where().eq("id", id).findUnique();
 
-					results[sideI] = ChatColor.AQUA + issue.getReported() + ChatColor.YELLOW + ", by " + ChatColor.AQUA + issue.getReporter() + ChatColor.YELLOW + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(issue.getTimestamp())) + ChatColor.YELLOW + ": " + ChatColor.AQUA + issue.getIssue();
+					results[sideI] = ChatColor.YELLOW + "[X] " + ChatColor.AQUA + issue.getReported() + ChatColor.YELLOW + ", by " + ChatColor.AQUA + issue.getReporter() + ChatColor.YELLOW + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(issue.getTimestamp())) + ChatColor.YELLOW + ": " + issue.getIssue();
 					break;
 				case BAN:
 					Bans ban = main.getDatabase().find(Bans.class).where().eq("id", id).findUnique();
 
-					results[sideI] = ChatColor.AQUA + ban.getBanned() + ChatColor.YELLOW + ", by " + ChatColor.AQUA + ban.getBanner() + ChatColor.YELLOW + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(ban.getTimestamp())) + ChatColor.YELLOW + ": " + ChatColor.AQUA + ban.getReason();
+					results[sideI] = ChatColor.DARK_RED + "[X] " + ChatColor.AQUA + ban.getBanned() + ChatColor.DARK_RED + ", by " + ChatColor.AQUA + ban.getBanner() + ChatColor.DARK_RED + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(ban.getTimestamp())) + ChatColor.DARK_RED + ": " + ban.getReason();
 					break;
 				case UNBAN:
 					Unbans unban = main.getDatabase().find(Unbans.class).where().eq("id", id).findUnique();
 
-					results[sideI] = ChatColor.AQUA + unban.getUnbanned() + ChatColor.YELLOW + ", by " + ChatColor.AQUA + unban.getUnbanner() + ChatColor.YELLOW + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(unban.getTimestamp())) + ChatColor.YELLOW + ": " + ChatColor.AQUA + unban.getReason();
+					results[sideI] = ChatColor.DARK_GREEN + "[X] " + ChatColor.AQUA + unban.getUnbanned() + ChatColor.DARK_GREEN + ", by " + ChatColor.AQUA + unban.getUnbanner() + ChatColor.DARK_GREEN + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(unban.getTimestamp())) + ChatColor.DARK_GREEN + ": " + unban.getReason();
 					break;
 				case PROMOTE:
 					Promotions promote = main.getDatabase().find(Promotions.class).where().eq("id", id).findUnique();
 
-					results[sideI] = ChatColor.AQUA + promote.getPromoted() + ChatColor.YELLOW + ", by " + ChatColor.AQUA + promote.getPromoter() + ChatColor.YELLOW + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(promote.getTimestamp())) + ChatColor.YELLOW + ": " + ChatColor.AQUA + promote.getReason();
+					results[sideI] = ChatColor.GREEN + "[X] " + ChatColor.AQUA + promote.getPromoted() + ChatColor.GREEN + ", by " + ChatColor.AQUA + promote.getPromoter() + ChatColor.GREEN + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(promote.getTimestamp())) + ChatColor.GREEN + ": " + promote.getReason();
 					break;
 				case DEMOTE:
 					Demotions demote = main.getDatabase().find(Demotions.class).where().eq("id", id).findUnique();
 
-					results[sideI] = ChatColor.AQUA + demote.getDemoted() + ChatColor.YELLOW + ", by " + ChatColor.AQUA + demote.getDemoter() + ChatColor.YELLOW + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(demote.getTimestamp())) + ChatColor.YELLOW + ": " + ChatColor.AQUA + demote.getReason();
+					results[sideI] = ChatColor.RED + "[X] " + ChatColor.AQUA + demote.getDemoted() + ChatColor.RED + ", by " + ChatColor.AQUA + demote.getDemoter() + ChatColor.RED + " at " + ChatColor.AQUA + dateFormat.format(Long.valueOf(demote.getTimestamp())) + ChatColor.RED + ": " + demote.getReason();
 					break;
 				default:
 					results[sideI] = ChatColor.DARK_GRAY + "Unspecified action happened";
