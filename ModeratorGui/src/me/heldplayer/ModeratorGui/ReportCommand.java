cd93eba4b6093f776ecf5f8689ba824a095e21a5
heldplayer <mitchel.pyl@gmail.com>
2012-05-06 15:17:27 +0200

    Added reporting promotions

--- ModeratorGui/src/me/heldplayer/ModeratorGui/ReportCommand.java
+++ ModeratorGui/src/me/heldplayer/ModeratorGui/ReportCommand.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 import me.heldplayer.ModeratorGui.tables.Bans;
 import me.heldplayer.ModeratorGui.tables.Issues;
+import me.heldplayer.ModeratorGui.tables.Promotions;
 import me.heldplayer.ModeratorGui.tables.Unbans;
 
 import org.bukkit.ChatColor;
@@ -169,6 +170,108 @@ public class ReportCommand implements CommandExecutor {
 
 			return true;
 		}
+		
+		if (args[0].equalsIgnoreCase("promote") && args.length > 4 && sender.hasPermission("moderatorgui.promote")) {
+			List<String> matchedNames = getPlayerMatches(args[1]);
+
+			String name = "";
+
+			if (matchedNames.size() == 1) {
+				name = matchedNames.get(0);
+			} else if (matchedNames.size() == 0) {
+				sender.sendMessage(ChatColor.RED + "No match found for '" + args[1] + "'");
+
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Multiple matches found: ");
+
+				String matches = "";
+
+				for (String matched : matchedNames) {
+					matches += ", " + matched;
+				}
+
+				matches = matches.replaceFirst(", ", "").trim();
+
+				sender.sendMessage(ChatColor.GRAY + matches);
+
+				return true;
+			}
+			
+			List<String> matchedRanks1 = getRankMatches(args[2]);
+
+			String rank1 = "";
+
+			if (matchedRanks1.size() == 1) {
+				rank1 = matchedRanks1.get(0);
+			} else if (matchedRanks1.size() == 0) {
+				sender.sendMessage(ChatColor.RED + "No match found for '" + args[2] + "'");
+
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Multiple matches found: ");
+
+				String matches = "";
+
+				for (String matched : matchedRanks1) {
+					matches += ", " + matched;
+				}
+
+				matches = matches.replaceFirst(", ", "").trim();
+
+				sender.sendMessage(ChatColor.GRAY + matches);
+
+				return true;
+			}
+			
+			List<String> matchedRanks2 = getRankMatches(args[3]);
+
+			String rank2 = "";
+
+			if (matchedRanks2.size() == 1) {
+				rank2 = matchedRanks2.get(0);
+			} else if (matchedRanks2.size() == 0) {
+				sender.sendMessage(ChatColor.RED + "No match found for '" + args[2] + "'");
+
+				return true;
+			} else {
+				sender.sendMessage(ChatColor.GREEN + "Multiple matches found: ");
+
+				String matches = "";
+
+				for (String matched : matchedRanks2) {
+					matches += ", " + matched;
+				}
+
+				matches = matches.replaceFirst(", ", "").trim();
+
+				sender.sendMessage(ChatColor.GRAY + matches);
+
+				return true;
+			}
+
+			String reason = "";
+
+			for (int i = 2; i < args.length; i++) {
+				reason += " " + args[i];
+			}
+
+			reason = reason.trim();
+
+			Promotions promotionRow = new Promotions();
+			promotionRow.setReason(reason);
+			promotionRow.setPromoted(name);
+			promotionRow.setPromoter(sender.getName());
+			promotionRow.setPrevRank(rank1);
+			promotionRow.setNewRank(rank2);
+			promotionRow.setTimestamp(System.currentTimeMillis());
+
+			main.getDatabase().save(promotionRow);
+			
+			sender.sendMessage(ChatColor.GREEN + "Reported!");
+
+			return true;
+		}
 
 		return false;
 	}
@@ -181,6 +284,20 @@ public class ReportCommand implements CommandExecutor {
 		for (OfflinePlayer player : players) {
 			if (player.getName().substring(0, name.length()).equalsIgnoreCase(name)) {
 				matched.add(player.getName());
+			}
+		}
+
+		return matched;
+	}
+	
+	private List<String> getRankMatches(String rank) {
+		List<String> ranks = main.ranks;
+
+		List<String> matched = new ArrayList<String>();
+
+		for (String matchedRank : ranks) {
+			if (matchedRank.substring(0, rank.length()).equalsIgnoreCase(rank)) {
+				matched.add(matchedRank);
 			}
 		}
 
